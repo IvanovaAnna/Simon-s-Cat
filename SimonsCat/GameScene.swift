68adb2b216c10fbfdca826b16f57dbb7c71ea914
Anna <annprokudina@gmail.com>
2017-08-09 11:48:52 +0300

    Adding items randomly;

--- SimonsCat/GameScene.swift
+++ SimonsCat/GameScene.swift
@@ -9,27 +9,96 @@
 import SpriteKit
 import GameplayKit
 
+//for the accelerometer
+import CoreMotion
+
 class GameScene: SKScene {
     
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
     var player: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        
+        configureStartScene ()
+        spawnObjectsOnTheFloor ()
+    }
+    
+    //add new objects
+    fileprivate func spawnObjectsOnTheFloor () {
+        //pause
+        let spawnObjectWait = SKAction.wait(forDuration: 6)
+        //add random object
+        let spawnObjectAction = SKAction.run {
+            let object = ObjectsOnTheFloor.populate()
+            self.addChild(object)
+        }
+        let spawnObjectSequence = SKAction.sequence([spawnObjectWait, spawnObjectAction])
+        let spawnObjectForever = SKAction.repeatForever(spawnObjectSequence)
+        run(spawnObjectForever)
+    }
+    
+    fileprivate func configureStartScene () {
         //add background
         self.backgroundColor = SKColor.white
         
-        
-        //add random point for items on the floor
         let screen = UIScreen.main.bounds
-        for _ in 1...5 {
-            let x: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
-            let y: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.height)))
-            let itemsOnTheFloor = ObjectsOnTheFloor.populateObjects(at: CGPoint(x: x, y: y))
-            self.addChild(itemsOnTheFloor)
-        }
+        
+        //add starting item (left bottom x: 75, y: 400)
+        let itemsOnTheFloor1 = ObjectsOnTheFloor.populate(at: CGPoint(x: 75, y: 400))
+        self.addChild(itemsOnTheFloor1)
+        
+        //add starting item (right top x: 100, y: 200)
+        let itemsOnTheFloor2 = ObjectsOnTheFloor.populate(at: CGPoint(x: self.size.width - 100, y: self.size.height - 50))
+        self.addChild(itemsOnTheFloor2)
+        
         
         //add a cat
         player = Cat.populate(at: CGPoint(x: screen.size.width / 2, y: 150))
         self.addChild(player)
+        
+        //how often to measure the acceleration
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            //data from sensors
+            if let data = data {
+                let acceleration = data.acceleration
+                //acceleration from inclination
+                //Coefficient for nonlinearity
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+            }
+        }
+    }
+    
+    override func didSimulatePhysics() {
+        super.didSimulatePhysics()
+        
+        //moving the cat
+        player.position.x += xAcceleration * 50
+        
+        //If the cat goes behind the screen - appears on the other side
+        if player.position.x < -70 {
+            player.position.x = self.size.width + 70
+        } else if player.position.x > self.size.width + 70 {
+            player.position.x = -70
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SimonsCat/ObjectsOnTheFloor.swift
+++ SimonsCat/ObjectsOnTheFloor.swift
@@ -11,14 +11,35 @@ import GameplayKit
 
 class ObjectsOnTheFloor: SKSpriteNode {
     
+    //add random point
+    static func randomPoint () -> CGPoint {
+        let screen = UIScreen.main.bounds
+        //above the screen for 100-200
+        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
+        let y = CGFloat(distribution.nextInt())
+        //from 0 to the width of the screen
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+        return CGPoint(x: x, y: y)
+    }
+    
     //add objects
-    static func populateObjects(at point: CGPoint) -> ObjectsOnTheFloor {
+    static func populate ( ) -> ObjectsOnTheFloor {
+        let objectImageName = configuraObjectsName ()
+        let object = ObjectsOnTheFloor(imageNamed: objectImageName)
+        object.setScale(0.8)
+        object.position = randomPoint()
+        object.zPosition = 1
+        object.run(move(from: object.position))
+        return object
+    }
+    
+    static func populate (at point: CGPoint ) -> ObjectsOnTheFloor {
         let objectImageName = configuraObjectsName ()
         let object = ObjectsOnTheFloor(imageNamed: objectImageName)
         object.setScale(0.8)
         object.position = point
         object.zPosition = 1
-        object.run(move(from: point))
+        object.run(move(from: object.position))
         return object
     }
     
@@ -34,7 +55,7 @@ class ObjectsOnTheFloor: SKSpriteNode {
     fileprivate static func move(from point: CGPoint) -> SKAction {
         let movePoint = CGPoint(x: point.x, y: -200)
         let moveDistance = point.y + 200
-        let movementSpeed: CGFloat = 10.0
+        let movementSpeed: CGFloat = 50.0
         let duratiom = moveDistance / movementSpeed
         return SKAction.move(to: movePoint, duration: TimeInterval(duratiom))
         
